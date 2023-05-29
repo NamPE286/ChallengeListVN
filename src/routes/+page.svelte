14df88d4493da48807496b567895db151a54f406
NamPE286 <nambuihung654@gmail.com>
2023-05-30 04:17:23 +0700

    add discord widget

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
 <script>
+    import Title from "../components/Title.svelte";
 </script>
+
+<Title value='Welcome back, name!'/>
+<iframe src="https://e.widgetbot.io/channels/725734982638895104/807166886390792252" height="350" width="500" title=''></iframe>
+
+<style lang='scss'>
+    iframe{
+        border: 1px solid var(--line);
+        margin-left: 50px;
+        margin-top: 20px;
+        border-radius: 15px;
+    }
+</style>
