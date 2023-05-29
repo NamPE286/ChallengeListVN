8d1b0ccefdc50e6986e5a62775cd23a820f5db2b
NamPE286 <nambuihung654@gmail.com>
2023-05-30 04:00:09 +0700

    Title component

---
+++ src/components/Title.svelte
@@ -0,0 +1,20 @@
+<script>
+    export var value
+</script>
+
+<div>
+    <h1>{value}</h1>
+</div>
+
+<style lang="scss">
+    div{
+        height: 130px;
+        background-color: black;
+        display: flex;
+        align-items: center;
+        border-bottom: 1px solid var(--line);
+    }
+    h1{
+        margin-left: 80px;
+    }
+</style>
