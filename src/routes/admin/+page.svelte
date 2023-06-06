32d9b57751d6ea5b4b18d507eb835a112f3e0bab
NamPE286 <nambuihung654@gmail.com>
2023-06-06 18:38:26 +0700

    update

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,21 +1,35 @@
 <script>
-    import Title from '../../components/Title.svelte'
-    import { user } from '../../stores'
+    import Title from "../../components/Title.svelte";
+    import { user } from "../../stores";
+    function refresh() {
+        fetch(`${import.meta.env.VITE_API_URL}/admin/refresh`, {
+            method: "POST",
+            headers: {
+                authorization: `Bearer ${$user.session.access_token}`,
+            },
+        }).then((res) => {
+            if(res.ok){
+                alert('Refreshed')
+            }
+        });
+    }
 </script>
 
 {#if $user && $user.isAdmin}
-    <Title value='Admin'/>
+    <Title value="Admin" />
     <main>
-        <a href='#!'>Level editor</a><br>
-        <a href='#!'>Record editor</a><br>
-        <a href='#!'>Notification sender</a><br>
+        <button on:click={refresh}>Refresh list</button><br />
+        <a href="#!">Level editor</a><br />
+        <a href="#!">Record editor</a><br />
+        <a href="#!">Notification sender</a><br />
         <span>Submit checker:</span>
-        <a href='#!'>Levels</a>
-        <a href='#!'>Records</a>
+        <a href="#!">Levels</a>
+        <a href="#!">Records</a>
     </main>
 {/if}
+
 <style lang="scss">
-    a{
+    a {
         text-decoration: underline;
         color: rgb(0, 153, 255);
     }
