aeab1ec681764ba2dc99191d23389448b0e30ea2
NamPE286 <nambuihung654@gmail.com>
2023-06-06 07:41:10 +0700

    pending submission done

--- src/components/PendingSubmission.svelte
+++ src/components/PendingSubmission.svelte
@@ -1,11 +1,48 @@
-<div class='wrapper'>
+<script>
+    import { user } from "../stores";
+    import { onMount } from "svelte";
+    var data = {
+        levels: [],
+        records: [],
+    };
+    $: $user && fetchData();
+    function fetchData() {
+        if (!$user) {
+            return;
+        }
+        fetch(`${import.meta.env.VITE_API_URL}/auth/pendingSubmission`, {
+            method: "POST",
+            headers: {
+                authorization: `Bearer ${$user.session.access_token}`,
+            },
+        })
+            .then((res) => res.json())
+            .then((dat) => {
+                data = dat;
+            });
+    }
+    onMount(() => {
+        fetchData();
+    });
+</script>
+
+<div class="wrapper">
     <h3>Pending Submission</h3>
-    <a href='#!'>Level's name by Creator</a><br>
-    <a href='#!'>Level's name by Creator</a><br>
+    <h4>Levels</h4>
+    {#each data.levels as item, index}
+        <a href="#!">{item.name} by {item.players.name}</a><br />
+    {/each}
+    <h4>Records</h4>
+    {#each data.records as item, index}
+        <a href="#!">{item.levels.name} by {item.levels.players.name}</a><br />
+    {/each}
 </div>
 
 <style lang="scss">
-    .wrapper{
+    h4{
+        margin-bottom: 10px;
+    }
+    .wrapper {
         height: fit-content;
         width: 500px;
         background-color: black;
@@ -15,12 +52,12 @@
         padding-inline: 25px;
         padding-bottom: 25px;
     }
-    a{
+    a {
         line-height: 20px;
         color: rgb(192, 192, 192);
     }
     @media screen and (max-width: 1000px) {
-        .wrapper{
+        .wrapper {
             width: 100%;
         }
     }
