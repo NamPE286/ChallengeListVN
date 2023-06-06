28b14ce9e8e976b3a8f164cc77f39821ab661213
NamPE286 <nambuihung654@gmail.com>
2023-06-06 15:46:09 +0700

    admin page

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -3,6 +3,19 @@
     import { user } from '../../stores'
 </script>
 
-{#if $user.isAdmin}
+{#if $user && $user.isAdmin}
     <Title value='Admin'/>
+    <main>
+        <a href='#!'>Level editor</a><br>
+        <a href='#!'>Record editor</a><br>
+        <span>Submit checker:</span>
+        <a href='#!'>Levels</a>
+        <a href='#!'>Records</a>
+    </main>
 {/if}
+<style lang="scss">
+    a{
+        text-decoration: underline;
+        color: rgb(0, 153, 255);
+    }
+</style>
