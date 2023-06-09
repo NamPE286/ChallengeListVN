8bffd02138a9e3d0a69857f017eeaca4a9228533
NamPE286 <nambuihung654@gmail.com>
2023-06-09 23:49:51 +0700

    update

--- src/routes/level/[id]/+page.svelte
+++ src/routes/level/[id]/+page.svelte
@@ -48,8 +48,8 @@
                 </section>
                 <p><b>Created by:</b><Badge data={level.data.players}><a href={`/player/${level.data.players.uid}`}>{level.data.players.name}</a></Badge></p>
                 <p><b>Submitted on: </b>{(new Date(level.data.timestamp)).toLocaleString('vi-VN')}</p>
-                <p><b>Length:</b> {level.data.length}</p>
-                <p><b>ID:</b> {lengthConv[level.data.length]}</p>
+                <p><b>Length:</b> {lengthConv[level.data.length]}</p>
+                <p><b>ID:</b> {level.data.id}</p>
                 <p><b>Description</b></p>
                 <p>{level.data.description}</p>
             </div>
