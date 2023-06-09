365ea8a47d7e5e4b1358082b227f5b19cfd9f6f7
NamPE286 <nambuihung654@gmail.com>
2023-06-09 23:36:48 +0700

    update rules

--- src/routes/rules/+page.svelte
+++ src/routes/rules/+page.svelte
@@ -5,5 +5,22 @@
 <Title value='Rules' />
 
 <main>
-    
+    <h2>General</h2>
+    <ul>
+        <li>If any level or record violate any rule, it will be deleted or rejected.</li>
+        <li>If a player violate too many rules in a short time, that player will be banned permanently or until any decision by staffs.</li>    
+    </ul>
+    <h2>Levels</h2>
+    <ul>
+        <li>Levels must be possible to beat and verified legitimately.</li>
+        <li>Levels must not have any secret way or fatal bug.</li>
+        <li>If any fatal bug is found, level must be updated in one week.</li>
+        <li>Levels must have decently good gameplay. This is determine by staff's opinion.</li>
+        <li>Levels must be longer than 10 seconds.</li>
+    </ul>
+    <h2>Record</h2>
+    <ul>
+        <li>Only 100% completions are accepted.</li>
+        <li>Tools, hacks, bugs, and secret ways are banned.</li>
+    </ul>
 </main>
