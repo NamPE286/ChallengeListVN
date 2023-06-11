61beec441de216f6be66b87f3438acd9c88417fd
NamPE286 <nambuihung654@gmail.com>
2023-06-12 03:50:25 +0700

    update

--- src/components/DailyLevel.svelte
+++ src/components/DailyLevel.svelte
@@ -24,7 +24,7 @@
         <Level data={level} mode='compact-fit' />
     {/if}
     <div class='text'><i>No level available</i></div>
-    <div class='text'>Reset every 00:00 UTC+7</div>
+    <div class='text'>Reset every 07:00 UTC+7</div>
 </div>
 
 <style lang="scss">
