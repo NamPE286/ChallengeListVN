0e2bdee26649cdce2255715bc1fab2c39dcfe257
NamPE286 <nambuihung654@gmail.com>
2023-08-11 17:21:56 +0700

    bug fix

--- src/routes/list/+page.svelte
+++ src/routes/list/+page.svelte
@@ -18,7 +18,7 @@
         },
         filter: {
             ascending: false,
-            sortBy: "id",
+            sortBy: "timestamp",
         },
     };
     var option = {
@@ -34,7 +34,7 @@
         },
         filter: {
             ascending: false,
-            sortBy: "id",
+            sortBy: "timestamp",
         },
     };
     function fetchData() {
@@ -114,7 +114,7 @@
                 <span>Sort by: </span>
                 <select name="sortBy" bind:value={option.filter.sortBy}>
                     <option value="rating">Rating</option>
-                    <option value="id">Date submitted</option>
+                    <option value="timestamps">Date submitted</option>
                 </select>
             </div>
             <div class="filterOptCheck">
